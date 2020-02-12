d88add5ad07c3ce13545840e5d761ecabec78736
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2020-02-12 12:10:38 -0600

    Adding features
    Added a rudimentary reporting feature. Currently only displays average severity of all bleeds.

--- Controllers/BleedsController.cs
+++ Controllers/BleedsController.cs
@@ -15,7 +15,35 @@ namespace NosebleedTrackerAlpha.Controllers
         {
             this.MySqlDatabase = mySqlDatabase;
         }
+
+        //Fetch the average severity of all bleeds for a user.
+        private dto.Report GetReport()
+        {
+
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+
+            cmd.CommandText = @"CALL spAverageSeverity(@user_id);";
+
+            //Right now the application only supports one user... User 0.
+            cmd.Parameters.AddWithValue("@user_id", 0);
+            
+            var reader = cmd.ExecuteReader();
+            
+            reader.Read();
+
+            var report = new dto.Report()
+            {
+                Average = decimal.ToInt32(reader.GetFieldValue<decimal>(0))
+            };
+
+            return report;
+
+        }
         
+        public IActionResult Report()
+        {
+            return View(this.GetReport());
+        }
 
     }
 }
