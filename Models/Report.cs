e926859db8098761401941a44b6ec8988006a5e1
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2020-02-12 12:11:43 -0600

    Added Report model
    Added a model to represent different kinds of reports. Right now only Average Severity is supported.

---
+++ Models/Report.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NosebleedTrackerAlpha.Models
+{
+    public class Report
+    {
+        public int Average { get; set; }
+
+    }
+}
