81cbfa81716ac215d5b34c61131c8ce00c805455
Carter Jones <36671987+cartermjones@users.noreply.github.com>
2020-02-14 12:18:42 -0600

    Refactoring, renaming, added report
    BleedsController renamed to ReportsController. Added report of frequency of bleeds in current month. Made some of the methods in the ReportsController a bit more modular.

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using dto = NosebleedTrackerAlpha.Models;
+
+namespace NosebleedTrackerAlpha.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private MySqlDatabase MySqlDatabase { get; set; }
+
+        public ReportsController(MySqlDatabase mySqlDatabase)
+        {
+            this.MySqlDatabase = mySqlDatabase;
+        }
+
+        //Fetch the average severity of all bleeds for a user.
+        private dto.Report GetAverage()
+        {
+
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+
+            cmd.CommandText = @"CALL spAverageSeverity(@user_id)";
+
+            //Right now the application only supports one user... User 0.
+            cmd.Parameters.AddWithValue("@user_id", 0);
+            
+            var reader = cmd.ExecuteReader();
+            
+            reader.Read();
+
+            var report = new dto.Report()
+            {
+                Average = decimal.ToInt32(reader.GetFieldValue<decimal>(0))
+            };
+
+            reader.Close();
+
+            return report;
+
+        }
+
+        //This will fetch the monthly frequency of bleeds for the current month.
+        private dto.Report GetFrequency()
+        {
+            var cmd = this.MySqlDatabase.Connection.CreateCommand();
+
+            cmd.CommandText = @"CALL spThisMonthBleedFrequency(@user_id)";
+
+            cmd.Parameters.AddWithValue("@user_id", 0);
+
+            var reader = cmd.ExecuteReader();
+
+            reader.Read();
+
+            var report = new dto.Report()
+            {
+                Frequency = reader.GetFieldValue<long>(0)
+            };
+
+            reader.Close();
+
+            return report;
+        }
+
+        //This aggregates the two above reports to pass into the View.
+        private dto.Report AggregateReport()
+        {
+            dto.Report average = GetAverage();
+            dto.Report frequency = GetFrequency();
+
+            var report = new dto.Report()
+            {
+                Average = average.Average,
+                Frequency = frequency.Frequency
+            };
+
+            return report;
+        }
+        
+        public IActionResult Reports()
+        {
+            return View(AggregateReport());
+        }
+
+    }
+}
